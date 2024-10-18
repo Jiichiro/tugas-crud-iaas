4683923cb0358bc3491ce48204883a62f12d422d
Jiichiro <cokmesoh@gmail.com>
2024-10-18 08:33:57 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -56,14 +56,14 @@
                     <span class="link_name">Cek Saldo</span>
                 </a>
                 <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Category</a></li>
+                    <li><a class="link_name" href="#">Edit Data</a></li>
                 </ul>
             </li>
             <li>
                 <div class="iocn-link">
                     <a href="#">
                         <i class='bx bx-collection'></i>
-                        <span class="link_name">Category</span>
+                        <span class="link_name">Edit Data</span>
                     </a>
                     <i class='bx bxs-chevron-down arrow'></i>
                 </div>
